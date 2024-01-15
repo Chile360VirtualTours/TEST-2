6ce8b307bf4c512c907bf5355355fdad59ef7d4d
Chile360VirtualTours <chile360virtualtours@gmail.com>
2024-01-14 22:42:38 -0600

    Upload

---
+++ data.js
@@ -0,0 +1,95 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20240115_011854_00_001_pureshot",
+      "name": "IMG_20240115_011854_00_001_PureShot",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.642162500817296,
+        "pitch": 0.42289394169951855,
+        "fov": 1.2597446215024068
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2535558012004593,
+          "pitch": 1.024970252966181,
+          "rotation": 6.283185307179586,
+          "target": "1-img_20240115_011936_00_002_pureshot"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-img_20240115_011936_00_002_pureshot",
+      "name": "IMG_20240115_011936_00_002_PureShot",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -3.0517272065835073,
+        "pitch": 0.2780736192692128,
+        "fov": 1.54848838965604
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20977999143284087,
+          "pitch": 1.0238798791861718,
+          "rotation": 0,
+          "target": "0-img_20240115_011854_00_001_pureshot"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "TEST 2",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
